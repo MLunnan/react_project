016c24fe6e40a5fb9cf44d9092ffcbda7f5b6de2
Marielle Lunnan <marielle.lunnan@learnet.se>
2022-09-15 15:38:32 +0200

    A start on responsiv page

--- src/App.js
+++ src/App.js
@@ -7,18 +7,17 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <Jokes category={category} />
-        <label>Choose Category</label>
-        <select value={category} onChange={(e) => setCategory(e.target.value)}>
-          <option value="hide">-- Category --</option>
-          <option value={"any"}>Any</option>
-          <option value={"programming"}>Programming</option>
-          <option value={"misc"}>Misc</option>
-          <option value={"pun"}>Pun</option>
-          <option value={"spooky"}>Spooky</option>
-        </select>
-      </header>
+      <header className="App-header">Wanna hear a joke?</header>
+      <label>Choose Category</label>
+      <select value={category} onChange={(e) => setCategory(e.target.value)}>
+        <option value="hide">-- Category --</option>
+        <option value={"any"}>Any</option>
+        <option value={"programming"}>Programming</option>
+        <option value={"misc"}>Misc</option>
+        <option value={"pun"}>Pun</option>
+        <option value={"spooky"}>Spooky</option>
+      </select>
+      <Jokes category={category} />
     </div>
   );
 }
