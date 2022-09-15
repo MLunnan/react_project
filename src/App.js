fa2a3c8b08d539fece9ae9349d311c64c4514cde
annazhutava <anna.zhutava@ecutbildning.se>
2022-09-15 13:03:47 +0200

    Created a drop-down list with categories

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import "./App.css";
+import { useState } from "react";
+import Jokes from "./components/Jokes.jsx";
 
 function App() {
+  const [category, setCategory] = useState();
+
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
+        <Jokes category={category} />
+        <label>Choose Category</label>
+        <select value={category} onChange={(e) => setCategory(e.target.value)}>
+          <option value="hide">-- Category --</option>
+          <option value={"any"}>Any</option>
+          <option value={"programming"}>Programming</option>
+          <option value={"misc"}>Misc</option>
+          <option value={"pun"}>Pun</option>
+          <option value={"spooky"}>Spooky</option>
+        </select>
+      </header>
     </div>
   );
 }
--- src/components/Jokes.jsx
+++ src/components/Jokes.jsx
@@ -1,32 +1,40 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from "react";
 
-const Jokes = ({category}) => {
-    const url = `https://v2.jokeapi.dev/joke/${category}`;
+const Jokes = ({ category }) => {
+  const url = `https://v2.jokeapi.dev/joke/${category}`;
 
-    const [data, setData] = useState();
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(false);
+  const [data, setData] = useState();
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
 
-    useEffect(() => {
-        fetch(url)
-        .then((response) => response.json())
-        .then((apiData) => {
-            setData(apiData)
-            setLoading(false)
-        })
-        .catch((err) => {
-            console.log(err)
-            setError(true)
-            setLoading(false)
-    })
-}, [])
-return (
+  useEffect(() => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((apiData) => {
+        setData(apiData);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setError(true);
+        setLoading(false);
+      });
+  }, [url]);
+  return (
     <div>
-        {loading ? (<div>loading...</div>) : (
-            error ? (<div>There was an error</div>) : 
-        (data.type === "single" ? (<div>{data.joke}</div>) : (<div>{data.setup}<div>{data.delivery}</div></div>))
-        )}
+      {loading ? (
+        <div>loading...</div>
+      ) : error ? (
+        <div>There was an error</div>
+      ) : data.type === "single" ? (
+        <div>{data.joke}</div>
+      ) : (
+        <div>
+          {data.setup}
+          <div>{data.delivery}</div>
+        </div>
+      )}
     </div>
-);
-}
+  );
+};
 export default Jokes;
