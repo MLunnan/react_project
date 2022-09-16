9e54ea469d0fd9ac4d99f281852960ed37e48a28
Marielle Lunnan <marielle.lunnan@learnet.se>
2022-09-16 09:52:03 +0200

    footer war

--- src/App.js
+++ src/App.js
@@ -12,25 +12,27 @@ function App() {
           Wanna read a joke?
         </h1>
       </header>
-      <div className="Text" id="display-box">
-        <label id="lable">Choose a Category</label>
-        <select value={category} onChange={(e) => setCategory(e.target.value)}>
-          <option value="hide">-- Category --</option>
-          <option value={"any"}>Any</option>
-          <option value={"programming"}>Programming</option>
-          <option value={"misc"}>Misc</option>
-          <option value={"pun"}>Pun</option>
-          <option value={"spooky"}>Spooky</option>
-        </select>
-      </div>
-      <div className="Text" id="display-box">
-        <Jokes category={category} />
-      </div>
-      <footer>
-        <div className="Text" id="footer-text">
-          &copy;React-Project by Simon, Anna Z & Marielle
+      <main>
+        <div className="Text" id="display-box">
+          <label id="lable">Choose a Category</label>
+          <select
+            className="Select"
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+          >
+            <option value="hide">-- Category --</option>
+            <option value={"any"}>Any</option>
+            <option value={"programming"}>Programming</option>
+            <option value={"misc"}>Misc</option>
+            <option value={"pun"}>Pun</option>
+            <option value={"spooky"}>Spooky</option>
+          </select>
         </div>
-      </footer>
+        <div className="Text" id="display-box">
+          <Jokes category={category} />
+        </div>
+      </main>
+      <footer>&copy; React_Project - Anna Z, Simon & Marielle</footer>
     </div>
   );
 }
