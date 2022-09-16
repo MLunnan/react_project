25cb4fa4b8137fa101e1292ff23e4565172d1750
Marielle Lunnan <marielle.lunnan@learnet.se>
2022-09-16 08:46:41 +0200

    responsive

--- src/App.js
+++ src/App.js
@@ -7,20 +7,30 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">Wanna hear a joke?</header>
-      <label>Choose Category</label>
-      <select value={category} onChange={(e) => setCategory(e.target.value)}>
-        <option value="hide">-- Category --</option>
-        <option value={"any"}>Any</option>
-        <option value={"programming"}>Programming</option>
-        <option value={"misc"}>Misc</option>
-        <option value={"pun"}>Pun</option>
-        <option value={"spooky"}>Spooky</option>
-      </select>
-      <div className="Textarea">
+      <header className="App-header">
+        <h1 className="Text" id="Header-text">
+          Wanna hear a joke?
+        </h1>
+      </header>
+      <div className="Text" id="display-box">
+        <label>Choose Category</label>
+        <select value={category} onChange={(e) => setCategory(e.target.value)}>
+          <option value="hide">-- Category --</option>
+          <option value={"any"}>Any</option>
+          <option value={"programming"}>Programming</option>
+          <option value={"misc"}>Misc</option>
+          <option value={"pun"}>Pun</option>
+          <option value={"spooky"}>Spooky</option>
+        </select>
+      </div>
+      <div className="Text" id="display-box">
         <Jokes category={category} />
       </div>
-      <footer>This is the footer</footer>
+      <footer>
+        <div className="Text" id="footer-text">
+          &copy;React-Project by Simon, Anna Z & Marielle
+        </div>
+      </footer>
     </div>
   );
 }
