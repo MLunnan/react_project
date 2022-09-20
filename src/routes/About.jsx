90b0fc29e887f3fcfa639df82a2f3c99dc73f1c5
Simon <simonbjornfors@protonmail.com>
2022-09-20 11:25:12 +0200

    routing

---
+++ src/routes/About.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div className="Text" id="display-box">
+            <h4>Api</h4>
+            <a href="https://sv443.net/jokeapi/v2/">JokeApi</a>
+            <h4>Github</h4>
+            <a href="https://github.com/MLunnan/react_project">react_project</a>
+            <a href="https://github.com/MLunnan">MLunnan</a>
+            <a href="https://github.com/simonbjornfors">simonbjornfors</a>
+            <a href="https://github.com/annazhutava">annazhutava</a>            
+        </div>
+    );
+}
+
+export default About;
